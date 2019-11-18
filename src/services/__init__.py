c68e4853e937427df72ff02da639f9f1f871cfd9
Subham <39151404+Subham2804@users.noreply.github.com>
2019-11-18 23:05:01 +0530

    update init

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,5 +1,5 @@
 import json
 from flask import make_response
 def nice_json(arg):
-    response = make_response(json.dumps(arg, sort_keys = True, indent=4))
+    response=make_response(json.dumps(arg, sort_keys = True, indent=4))
    
